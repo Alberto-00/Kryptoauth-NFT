87cd24d3ec049f49f09b6487943dde8403460803
Alberto-00 <albmont99@gmail.com>
2022-06-07 14:00:33 +0200

    Update 2_deploy_contracts.js

--- authentication DApp/migrations/2_deploy_contracts.js
+++ authentication DApp/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var Authentication = artifacts.require("./Authentication.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(Authentication, '0xDFdfB19AaC836406BEE74cfb0003Cb49ead319A5');
+  deployer.deploy(Authentication, '0x0fD737aFb101884A864E96c8943D8B7c15Ec3C4A');
 };
