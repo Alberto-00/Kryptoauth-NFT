afa97d58514c746940e610c4a6724beed255302e
Alberto-00 <albmont99@gmail.com>
2022-06-07 14:00:29 +0200

    Update Authentication.sol
    fix bug accessControl

--- authentication DApp/contracts/Authentication.sol
+++ authentication DApp/contracts/Authentication.sol
@@ -1,13 +1,16 @@
 // SPDX-License-Identifier: MIT
 
 /********************************************************************************************** 
-* Questo smart contract implementa una configurazione tradizionale tra admin e utenti:        *
+* Questo smart contract implementa una configurazione tra admin e utenti:                     *
 *                                                                                             *
 * -  DEFAULT_ADMIN_ROLE è il ruolo di admin di USER (default);                                *
-* -  l'inidirizzo passato al costruttore è l'admin iniziale;                                  *
+* -  l'indirizzo passato al costruttore è l'admin iniziale;                                   *
 * -  gli admin possono aggiungere altri amministratori;                                       *
 * -  gli admin possono concedere e revocare le autorizzazioni utente a qualsiasi account;     *
-* -  l'unico modo per un admin di perdere il proprio ruolo di admin è di rinunciarvi.            *
+* -  l'unico modo per un admin di perdere il proprio ruolo di admin è di rinunciarvi.         *
+*                                                                                             *
+* Quando un utente o un admin si registrano entrambi non hanno un ruolo che verrà asseggnato  *
+* in seguito dall'admin con "addUser" o "addAdmin".                                           *
 *                                                                                             *
 ***********************************************************************************************/
 
@@ -75,33 +78,29 @@ contract Authentication is AccessControl {
         renounceRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    /*
-    * Nella funzione di registrazione dell'utente, se l'utente non si è già 
+
+    /************************************************************************
+    * Nella funzione di registrazione dell'utente, se l'utente non si è già  
     * registrato i suoi dati vengono registrati sulla blockchain.
     */
     function registerUser(
         address _address,
         string memory _name,
         string memory _password
-    ) public onlyUser returns (bool) {
-        require(
-            user[_address].addr == address(0x0000000000000000000000000000000000000000), // indirizzo eth nullo
-            "already registered"
-        );
+    ) public returns (bool) {
+        require(user[_address].addr == address(0x0000000000000000000000000000000000000000), "already registered");
         user[_address].addr = _address;
         user[_address].name = _name;
         user[_address].password = keccak256(abi.encodePacked(_password));
         user[_address].isUserLoggedIn = false;
-        addUser(_address);
         return true;
     }
 
-    
-    /**
-    * La funzione di login fornisce la possibilità di accedere alla DApp e
-    * la modifica del campo isUserLoggedIn su true: questo significa che 
-    * l'utente accede correttamente. 
-    */
+    /***********************************************************************
+    * La funzione di login fornisce la possibilità di accedere alla DApp   *
+    * e la modifica del campo isUserLoggedIn su true: questo significa che *
+    * l'utente accede correttamente.                                       *
+    ************************************************************************/
     function loginUser(address _address, string memory _password) public returns (bool) {
         if (user[_address].password == keccak256(abi.encodePacked(_password))) {
             user[_address].isUserLoggedIn = true;
@@ -132,16 +131,12 @@ contract Authentication is AccessControl {
         address _address,
         string memory _name,
         string memory _password
-    ) public onlyAdmin returns (bool) {
-        require(
-            admin[_address].adminAddress == address(0x0000000000000000000000000000000000000000), // indirizzo eth nullo
-            "already registered"
-        );
+    ) public returns (bool) {
+        require(admin[_address].adminAddress == address(0x0000000000000000000000000000000000000000), "already registered");
         admin[_address].adminAddress = _address;
         admin[_address].name = _name;
         admin[_address].password = keccak256(abi.encodePacked(_password));
         admin[_address].isAdminLoggedIn = false;
-        addAdmin(_address);
         return true;
     }
 
